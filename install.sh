0770494674481b1787ad78132e10cb7548614bce
Marek <marek.dec@sap.com>
2020-02-28 09:17:06 +0100

    improvement cp replaced with soft link added some additional checks

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-echo "**************************************************************************"
-echo "Before you will use this script you have to install kubectl KREW plugin"
-echo "**************************************************************************"
-echo "This script will copy k8s-plugin to /usr/local/bin/ which is scanned by KREW plugin"
+echo "This script will create following links:"
+echo " - to  k8s-plugin in /usr/local/bin/ which is scanned by KREW plugin"
+echo " - to /opt/k8s/tools/lib and /opt/k8s/tools/bin"
 
-cp ./k8s-plugin/kubectl-tools_on /usr/local/bin/
-echo "modify .bashrc and .bash_profile"
-tools_location=$(pwd)
-echo "export tools_folder=$tools_location" >> $HOME/.bashrc
-echo "export tools_folder=$tools_location" >> $HOME/.bash_profile
+if [ -f "/usr/local/bin/kubectl-krew" ]
+	then
+		echo "krew is installed"
+	else
+		echo "**************************************************************************"
+		echo "Before you will use this script you have to install kubectl KREW plugin"
+		echo "**************************************************************************"
+		exit 1
+		
+fi		
+
+
+current_folder=$(pwd)
+ln -s $current_folder/k8s-plugin/kubectl-tools_on /usr/local/bin/kubectl-tools_on
+mkdir -p /opt/k8s/tools
+ln -s $current_folder/bin /opt/k8s/tools/bin
+ln -s $current_folder/lib /opt/k8s/tools/lib
